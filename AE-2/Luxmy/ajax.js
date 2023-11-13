b76a6eb979d0a3335f60d894a48743335b5ae58c
nlarunasalam <nlarunasalam@gmail.com>
2023-11-13 08:18:58 +0100

    requerimiento1

---
+++ AE-2/Luxmy/ajax.js
@@ -0,0 +1,70 @@
+// Simulamos BD y URL de destino
+const URL = "http://127.0.0.1:5500/AE-2/Luxmy/";
+const RECURSO = "pizzeria.json";
+
+
+function cargarDatos() {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', URL + RECURSO, true);
+    xmlHttp.send(null);
+
+    xmlHttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                procesarRespuesta(this.responseText);
+            } else {
+                alert("¡Error en la obtención de datos!");            }
+        }
+    };
+}
+
+cargarDatos();
+
+// Esta función carga los datos de los ingredientes y tamaños
+function procesarRespuesta(jsonDoc) {
+    let objetoJson = JSON.parse(jsonDoc);
+
+    // Cargar tamaños
+    let tamañoContenedor = document.getElementById('tamañoContenedor');
+    tamañoContenedor.innerHTML = '';
+
+    objetoJson.pizzeria.tamaños.forEach(tamaño => {
+        let input = document.createElement('input');
+        input.name = 'tamaños';
+        input.type = 'radio';
+        input.value = tamaño.nombre;
+
+        let label = document.createElement('label');
+        label.textContent = `${tamaño.nombre} (+${tamaño.precio}€)`;
+
+        let br = document.createElement('br');
+
+        tamañoContenedor.appendChild(input);
+        tamañoContenedor.appendChild(label);
+        tamañoContenedor.appendChild(br);
+    });
+
+    // Cargar ingredientes
+    let ingredientesContenedor = document.getElementById('ingredientesContenedor');
+    ingredientesContenedor.innerHTML = ''; 
+
+    objetoJson.pizzeria.ingredientes.forEach(ingrediente => {
+        let input = document.createElement('input');
+        input.name = 'ingredientes';
+        input.type = 'checkbox';
+        input.value = ingrediente.nombre;
+
+        let label = document.createElement('label');
+        label.textContent = `${ingrediente.nombre} (+${ingrediente.precio}€)`;
+
+        let br = document.createElement('br');
+
+        ingredientesContenedor.appendChild(input);
+        ingredientesContenedor.appendChild(label);
+        ingredientesContenedor.appendChild(br);
+    });
+
+    function refrescarDatos() {
+        cargarDatos();
+    }
+}
